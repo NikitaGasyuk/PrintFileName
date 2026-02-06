1add5bb2339e9a9a3281803a36cdb979c48c26ce
NikitaGasyuk <126286242+NikitaGasyuk@users.noreply.github.com>
2026-02-06 17:32:30 +0300

    Update PrintFileName.cpp

--- PrintFileName.cpp
+++ PrintFileName.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <dirent.h>
 
-int main() {
-    int n = -2;
+int main() 
+{
+    // инициализация переменной порядкового счёта файлов в директории(папке)
+    int n = 0;
+    // Переменная в которую будет записана открытая директория(путь к файлу) 
     DIR *dir;
+    // структура содержащая информацию об элементе директории
     struct dirent *entry;
+    // проверка на то что путь к открываемой директории существует или правильно указан
     if ((dir = opendir("PATH")) != NULL) 
     {
+        // цикл по всем файлам в папке, проверка на то что файл существует
         while ((entry = readdir(dir)) != NULL)
         {
+            // вывод порядкового номера файла и его названия
             n = n + 1;
-            std::cout << n << " "; 
+            std::cout << n << " - "; 
             std::cout << entry->d_name << std::endl;
         }
-    closedir(dir);
+        //закрытие директории
+        closedir(dir);
     } 
     else 
     {
-    perror("Could not open directory");
-    return EXIT_FAILURE;
+        // вывод ошибки в случае если с директорией что-то не так
+        perror("Could not open directory");
+        return EXIT_FAILURE;
     }
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 } 
